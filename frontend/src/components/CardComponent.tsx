89f460906bbeb3251de71080470bf54786f97ace
Roman <woody.falcone.00@gmail.com>
2024-08-12 23:47:22 +0300

    delete function implementation

--- frontend/src/components/CardComponent.tsx
+++ frontend/src/components/CardComponent.tsx
@@ -8,7 +8,7 @@ interface Card {
 
 const CardComponent: React.FC<{ card: Card }> = ({ card }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-2 mb-2 hover:bg-gray-400 transition-all duration-300">
+    <div className="bg-white shadow-lg rounded-lg p-2 mb-2 hover:bg-gray-300 transition-all duration-300">
       <h2 className="text-sm text-gray-600">ID: {card.id}</h2>
       <h3 className="text-lg font-semibold text-gray-800">{card.name}</h3>
       <p className="text-md text-gray-700">{card.email}</p>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -34,6 +34,26 @@ export default function Home() {
     fetchUsers();
   }, []);
 
+  //create new user
+  const createUser = async () => {
+    try {
+      const response = await axios.post(`${apiUrl}/users`, newUser);
+      setUsers([...users, response.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //delete user
+  const deleteUser = async (id: number) => {
+    try {
+      await axios.delete(`${apiUrl}/users/${id}`);
+      setUsers(users.filter((user) => user.id !== id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
       <div className="space-y-4 w-full max-w-2xl">
@@ -41,18 +61,24 @@ export default function Home() {
           User Managment
         </h1>
 
-
-      {/* Display all users */}
+        {/* Display all users */}
 
         {users.map((user) => (
-          <CardComponent
-            key={user.id}
-            card={user}
-            setUpdateUser={setUpdateUser}
-            setUsers={setUsers}
-          />
+          <div className="space-y-2">
+            <div
+              key={user.id}
+              className="flex items-center justify-between bg-white shadow-lg rounded-lg p-2 mb-2"
+            >
+              <CardComponent card={user} />
+              <button
+                onClick={() => deleteUser(user.id)}
+                className="shadow-lg rouded-lg p-2 m-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded transition-all duration-300"
+              >
+                Delete User
+              </button>
+            </div>
+          </div>
         ))}
-
       </div>
     </main>
   );
